2f64a6ef9a6c232cd39007ca8038d067a4a83c7f
souemon16 <souemon16@gmail.com>
2021-07-02 20:38:57 +0600

    sign-in route created for server

--- server/router/auth.js
+++ server/router/auth.js
@@ -12,32 +12,48 @@ router.get('/about', (req, res) => {
     res.send("About page from auth.js")
 });
 
-router.post('/signup', (req, res) => {
+router.post('/sign-up', async (req, res) => {
     const { name, email, phone, work, password, cpassword } = req.body;
 
     if (!name || !email || !phone || !work || !password || !cpassword) {
         res.status(422).json({ error: "This field can't be empty. Please fill it out" })
     }
 
-    User.findOne({ email: email })
-        .then((userExist) => {
-            if (userExist) {
-                res.status(422).json({ error: "This email is already exist. Please Sign In." })
+    try {
+        const userExist = await User.findOne({ email: email });
+        if (userExist) {
+            res.status(422).json({ error: "This email is already exist. Please Sign In." })
+        } else {
+            const user = new User({ name, email, phone, work, password, cpassword });
+            const registerUser = await user.save();
+            if (registerUser) {
+                res.status(201).json({ message: "Successfully created new account" });
             } else {
-                const user = new User({ name, email, phone, work, password, cpassword });
-                user.save()
-                    .then(() => {
-                        res.status(201).json({ message: "Successfully created new account" });
-                    })
-                    .catch((error) => {
-                        res.status(500).json({ error: "Failed to created your account" });
-                        console.log(error);
-                    })
+                res.status(500).json({ error: "Failed to created your account" });
             }
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+        }
+    } catch (error) {
+        console.log(error);
+    }
 });
+
+router.post('/sign-in', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+        res.status(422).json({ error: "This field can't be empty. Please fill it out" })
+        }
+        const userExist = await User.findOne({ email: email });
+        if(!userExist){
+            res.status(400).json({message: "No user found."});
+        } else {
+            res.json({message: "user find successfully"});
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 module.exports = router;
