00389b4c106c5e9fc9e981c9e720caa4321cc860
souemon16 <souemon16@gmail.com>
2021-07-05 00:12:41 +0600

    sign in and sign up client side created

--- client/src/Components/SignIn/SignIn.jsx
+++ client/src/Components/SignIn/SignIn.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import './SignIn.css';
+import signInImage from '../../Resources/Images/signin-image.jpg';
 
 const SignIn = () => {
     return (
-        <div>
-        <h1>Welcome from Sign in Page</h1>
-        </div>
+        <React.Fragment>
+            <div className="signin-container">
+            <div className="signin-form">
+              <div className="heading">
+                <h1>Login Form</h1>
+              </div>
+              <div className="wrap2">
+                <label htmlFor="email">E-mail</label>
+                <input type="email" name="email" id="email" />
+                <span className="focus-input"></span>
+              </div>
+              <div className="wrap2">
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="password" />
+                <span className="focus-input"></span>
+              </div>
+              <button className="sign-btn" type="submit">Sign In</button>
+              <p className="signup-para">Don't Have a Account. Please Sign Up</p>
+            </div>
+            <div className="image">
+                <img src={signInImage} alt="SignIn Image" className="img" />
+                {/* <p className="login-para2">Don't Have a Account. Please Sign Up</p> */}
+              </div>
+          </div>
+        </React.Fragment>
     )
 }
 
--- client/src/Components/SignUp/SignUp.jsx
+++ client/src/Components/SignUp/SignUp.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import React from "react";
+import "./SignUp.css";
+import signUpImage from "../../Resources/Images/signup-image.jpg";
 
 const SignUp = () => {
-    return (
-        <div>
-        <h1>Welcome from Sign Up Page</h1>
+  return (
+    <React.Fragment>
+      <div className="signup-container">
+        <div className="signup-form">
+          <div className="heading">
+            <h1>Registration Form</h1>
+          </div>
+          <div className="wrap2">
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" />
+            <span className="focus-input"></span>
+          </div>
+          <div className="wrap2">
+            <label htmlFor="email">E-mail</label>
+            <input type="email" name="email" id="email" />
+            <span className="focus-input"></span>
+          </div>
+          <div className="wrap">
+            <div className="wrap2 f1">
+              <label htmlFor="phone">Phone</label>
+              <input type="number" name="phone" id="phone" />
+              <span className="focus-input"></span>
+            </div>
+            <div className="wrap2 f2">
+              <label htmlFor="work">Work</label>
+              <input type="text" name="work" id="work" />
+              <span className="focus-input"></span>
+            </div>
+          </div>
+          <div className="wrap2">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" />
+            <span className="focus-input"></span>
+          </div>
+          <div className="wrap2">
+            <label htmlFor="confirmpassword">Confirm Password</label>
+            <input
+              type="password"
+              name="Confirm Password"
+              id="confirmpassword"
+            />
+            <span className="focus-input"></span>
+          </div>
+          <button className="sign-btn" type="submit">
+            Sign Up
+          </button>
+          <p className="login-para">Already Have a Account. Please Log In</p>
         </div>
-    )
-}
+        <div className="image">
+          <img src={signUpImage} alt="SignUp Image" className="img" />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
-export default SignUp
+export default SignUp;
